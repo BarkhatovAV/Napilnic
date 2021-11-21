cbd389d02717b8ed8144fb75e121da71e425af18
BarkhatovAV <barkhatov.a.v@mail.ru>
2021-11-21 11:48:19 +0300

    init

---
+++ Task01/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Weapon
+{
+    class Weapon
+    {
+        private int _damage;
+        private int _bulletCount;
+
+        public void Fire(Player player)
+        {
+            if(_bulletCount > 0)
+            {
+                player.TakeDamage(_damage);
+                _bulletCount -= 1;
+            }
+        }
+    }
+
+    class Player
+    {
+        private int _health;
+
+        public void TakeDamage(int damage)
+        {
+            if(damage > 0)
+            {
+                _health -= damage;
+            }
+        }
+    }
+
+    class Bot
+    {
+        private Weapon _weapon;
+
+        public void OnSeePlayer(Player player)
+        {
+            _weapon.Fire(player);
+        }
+    }
+}
